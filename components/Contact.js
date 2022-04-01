d8f07ad0516d5fdff06e30645365789ddb9a62fc
Hordon13 <hordonandron@gmail.com>
2022-04-01 22:33:22 +0200

    eslint fix

--- components/Contact.js
+++ components/Contact.js
@@ -9,8 +9,9 @@ const Contact = () => {
           <p>
             If you have any questions about the working process, prices, or
             other details, you may find answers on our FAQ page. <br /> <br />
-            Please feel free to contact us at any time. Don't worry if you don't
-            know exactly what you want, we will help you figure it out.
+            Please feel free to contact us at any time. Don&apos;t worry if you
+            don&apos;t know exactly what you want, we will help you figure it
+            out.
           </p>
           <a href="mailto:page@notfound.hu">send an email</a>
         </div>
